4df4acf796e31f81e67721f4f7825c08d91de12f
Emmanuel Telmon <etelmon@zimit.io>
2016-12-19 21:43:19 -0500

    Fix circular dependency in object - missing unit tests to confirm array references are maintained properly

--- sync/services/sync-merge.service.js
+++ sync/services/sync-merge.service.js
@@ -32,25 +32,17 @@ function syncMerge() {
         return updateObject(destination, source, isStrictMode);
 
 
-        function findProcessed(value) {
+        function findObjectReference(value) {
             if (!_.isArray(value) && !_.isObject(value) && !_.isDate(value) && !_.isFunction(value)) {
                 return null;
             }
             var found = _.find(processed, function (p) {
                 return value === p.value;
             });
-            // return null;
-            // if (found) {
-            //     if (!found.processed) {
-            //         found.processed = true;
-            //         return null;
-            //     } else {
-            //         return found.newValue;
-            //     }
-            // }
-            // return null;
-            return found;//? found.newValue : null;
+            return found ? found.newValue : null;
         }
+
+        
 
         function updateObject(destination, source, isStrictMode) {
             if (!destination) {
@@ -70,39 +62,49 @@ function syncMerge() {
             // create new object containing only the properties of source merge with destination
             var newValue, object = {};
             for (var property in source) {
-                var processedData = findProcessed(source[property]);
+                var reference = findObjectReference(source[property]);
 
                 //Object.getOwnPropertyDescriptor                
                 var d = Object.getOwnPropertyDescriptor(source, property);
                 if (d && (d.set || d.get)) {
                     // do nothing, it is computed
 
-                } else if (processedData) {
-                    object[property] = destination[property];//processedData;
+                } else if (reference) {
+                    // if the object in the source was already processed,
+                    // let's use the reference
+                    object[property] = reference;//destination[property];
 
                 } else if (_.isArray(source[property])) {
-                    newValue = object[property] = updateArray(destination[property], source[property], isStrictMode);
-                    processed.push({ value: source[property], newValue: newValue });
 
-
-                } else if (_.isFunction(source[property])) {
-                    //      object[property] = source[property];
-
-                    //       processed.push({ value: source[property], newValue: object[property] });
-
-                    // should do nothing...no function be added!
-
-                } else if (_.isObject(source[property]) && !_.isDate(source[property])) {
+                    // if an array is multiple time references in the source
+                    // make sure we will also reference multiple time the same object in the destination
                     if (destination[property]) {
-                        processed.push({ value: source[property], newValue: object[property] });
-                        object[property] = updateObject(destination[property], source[property], isStrictMode);
+                        processed.push({ value: source[property], newValue: destination[property] });
+                        object[property] = updateArray(destination[property], source[property], isStrictMode);
                     } else {
                         processed.push({ value: source[property], newValue: source[property] });
                         object[property] = source[property];
                     }
 
 
+                } else if (_.isFunction(source[property])) {
+                    // Function are not merged. Do nothing.
+
+                } else if (_.isObject(source[property]) && !_.isDate(source[property])) {
+                    if (destination[property]) {
+                        // the related object exists in the destination,
+                        // let's update it.
+                        // 
+                        // so next time, a source property deals with this object, there will be no need to merge it.
+                        processed.push({ value: source[property], newValue: destination[property] });
+                        object[property] = updateObject(destination[property], source[property], isStrictMode);
+                    } else {
+                        processed.push({ value: source[property], newValue: source[property] });
+                        object[property] = source[property];
+                    }
+
                 } else {
+                    // a basic property value is just copied.
                     object[property] = source[property];
                 }
             }
@@ -120,9 +122,10 @@ function syncMerge() {
             var array = [];
             source.forEach(function (item) {
 
-                var processedData = findProcessed(item);
-                if (processedData) {
-                    array.push(processedData);
+                var processedReference = findObjectReference(item);
+                if (processedReference) {
+                    // if an object is already referenced, let's use the same reference;
+                    array.push(processedReference);
                 } else
 
                     // does not try to maintain object references in arrays
@@ -137,8 +140,12 @@ function syncMerge() {
                                 var dest = _.find(destination, function (obj) {
                                     return obj.id.toString() === item.id.toString();
                                 });
+
+                                // save reference to an object for reuse;                                
+                                processed.push({ value: item, newValue: dest });                                
+
                                 array.push(updateObject(dest, item, isStrictMode));
-                                processed.push({ value: item, newValue: dest });
+                                
                             } else {
                                 if (isStrictMode) {
                                     throw new Error('objects in array must have an id otherwise we can\'t maintain the instance reference. ' + stringify(item));
